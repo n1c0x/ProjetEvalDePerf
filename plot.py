d77dcd4e2d8a46081dc48b0a9fad0d19ad22e83b
n1c0x <nicolas.renard67@gmail.com>
2015-12-09 20:30:58 +0100

    affichage de l'horodatage des fichiers perdus

--- plot.py
+++ plot.py
@@ -4,19 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-#files = {}
+files = {}
 
 #for filename in os.listdir(os.getcwd()+'/traces_small/') :
-	#files.append(filename)
-	#trace = open('traces_small/'+filename, "r")
-	#files["trace" + str(filename)] = trace.readlines()
+#	files[filename] = filename
+#	trace = open('traces_small/'+filename, "r")
+#	files["trace" + str(filename)] = trace.readlines()
 
-#print(files)
+	#print(files['queue017.tr'])
+
 
 #for filename in files : 
 trace = open('traces_small/queue017.tr', "r")
 lignes_traces = trace.readlines()
 
+contenu = []
+
+id_trace = 0
 action = []
 temps = []
 noeud_src = []
@@ -30,9 +34,16 @@ addr_noeud_dst = []
 seq = []
 id_paquet = []
 
+nb_paquets_perdus = []
+
 for ligne in lignes_traces :
 	contenu = np.array(ligne.split())
-	
+	if contenu[0] == 'd':
+		nb_paquets_perdus.append(contenu[1])
+		#print("Paquet perdu à "+contenu[1])
+
+
+
 	action.append(contenu[0])
 	temps.append(contenu[1])
 	noeud_src.append(contenu[2])
@@ -46,10 +57,13 @@ for ligne in lignes_traces :
 	seq.append(contenu[10])
 	id_paquet.append(contenu[11])
 
+
+print("Nombre de paquets perdus: "+str(nb_paquets_perdus))
+
 # definition de l'échelle (log ou pas)
 # plt.yscale('log')
 
-plt.plot(temps, seq)
+plt.plot(temps, nb_paquets_perdus)
 plt.ylabel('Test')
 plt.xlabel('Temps')
 plt.show()
--- script.py
+++ script.py
@@ -97,7 +97,7 @@ script.write('\n\n')
 
 i = 0
 while i < nb_liens:
-	script.write('$ns duplex-link $n('+str(noeuds_tries_orig[i])+') $n('+str(noeuds_extr[i])+') '+str(capacite[i])+'Mb '+str(delai[i])+'ms DropTail\n')
+	script.write('$ns duplex-link $n('+str(noeuds_tries_orig[i])+') $n('+str(noeuds_extr[i])+') '+str(capacite[i])+'Gb '+str(delai[i])+'ms DropTail\n')
 	script.write('set file'+str(noeuds_tries_orig[i])+str(noeuds_extr[i])+' [open traces/queue'+str(noeuds_tries_orig[i])+str(noeuds_extr[i])+'.tr w]\n')
 	script.write('$ns trace-queue $n('+str(noeuds_tries_orig[i])+') $n('+str(noeuds_extr[i])+') $file'+str(noeuds_tries_orig[i])+str(noeuds_extr[i])+'\n')
 	script.write('$ns queue-limit $n('+str(noeuds_tries_orig[i])+') $n('+str(noeuds_extr[i])+') 10\n\n')
@@ -131,12 +131,15 @@ while compteur_flux < nb_flux:
 	segmentSize = 0
 	compteur_volume = 0
 	for compteur_volume in liste_total_volume:
-		compteur_volume = compteur_volume.astype(int)*1000000*8
-		#compteur_volume = compteur_volume.astype(int)
+		#compteur_volume = compteur_volume.astype(int)*1024*1024*8
+		compteur_volume = compteur_volume.astype(int)
 		iterateur_sous_flux = 0
-		while somme_volume < compteur_volume :
+		while somme_volume < compteur_volume*1024*1024*8 :
 			taille_segment_tcp = np.random.zipf(a, 1)
-			segmentSize = taille_segment_tcp[0]
+			#print(np.int(taille_segment_tcp[0])*1024*1024*8)
+
+			segmentSize = np.int(taille_segment_tcp[0])
+			#segmentSize = taille_segment_tcp[0]
 
 			script.write('set null('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') [new Agent/TCPSink]\n')
 			script.write('$ns attach-agent $n('+str(noeuds_traf_src[compteur_flux])+') $null('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+')\n')
@@ -147,7 +150,7 @@ while compteur_flux < nb_flux:
 
 			script.write('set ftp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') [new Application/FTP]\n')
 			script.write('$ftp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') attach-agent $tcp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+')\n')
-			script.write('$ns at '+str(rand_duree())+' "$ftp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') start"\n')
+			script.write('$ns at '+str(rand_duree())+' "$ftp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') send '+str(segmentSize)+'"\n')
 			#script.write('$ns at '+str(rand_duree())+' "$tcp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') send size '+str(segmentSize)+'"\n')
 			script.write('$ns at '+str(4.5)+' "$ftp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') stop"\n\n')
 			somme_volume = somme_volume + taille_segment_tcp[0]
