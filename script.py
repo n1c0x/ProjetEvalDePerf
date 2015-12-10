ef10a1495171dbae388ebbdd9c767a65b2b6c2bd
n1c0x <nicolas.renard67@gmail.com>
2015-12-10 22:52:44 +0100

    simulation qui marche

--- script.py
+++ script.py
@@ -6,9 +6,8 @@ from scipy.stats import zipf
 
 
 def rand_duree() :
-	return np.random.random()*4
+	return np.random.random()*240
 
-#liste_total_volume = 500000
 a=1.2
 np.set_printoptions(precision=3)
 i = 0
@@ -24,18 +23,7 @@ set ns [new Simulator]
 # protocole de routage DV: distance vector LS: link state
 $ns rtproto DV
 
-#set f [open my.dat w]
-#$ns trace-all $f
-#set nf [open out2.nam w]
-#$ns namtrace-all $nf
-
 proc finish {} {
-	#global ns trace_all
-	#$ns flush-trace
-	#Close the NAM trace file
-	#close $trace_all
-	#Execute NAM on the trace file
-	#exec nam -a out2.nam &
 	exit 0
 }
 
@@ -76,47 +64,30 @@ noeuds_tries_orig_uniques.sort(key=int)
 # Transformation de string vers int
 noeuds_tries_orig_uniques = [int(i) for i in noeuds_tries_orig_uniques]
 
-# initialisation de la taille du tableau
-# nb_liens = len(noeuds_tries_orig_uniques)
 
 while i < nb_noeuds:
 	script.write('set n(')
-	#script.write(str(noeuds_tries_orig_uniques[i]))
 	script.write(str(i))
 	script.write(""") [$ns node]
 """)
 		
 	i += 1
 
-#nb_liens = 0
 noeuds_tries_orig = [int(i) for i in noeuds_tries_orig]
 nb_liens = len(noeuds_tries_orig)
 
-# Ligne blanche
 script.write('\n\n')
 
 i = 0
 while i < nb_liens:
-	script.write('$ns duplex-link $n('+str(noeuds_tries_orig[i])+') $n('+str(noeuds_extr[i])+') '+str(capacite[i])+'Mb '+str(delai[i])+'ms DropTail\n')
+	script.write('$ns duplex-link $n('+str(noeuds_tries_orig[i])+') $n('+str(noeuds_extr[i])+') '+str(capacite[i])+'Gb '+str(delai[i])+'ms DropTail\n')
 	script.write('set file'+str(noeuds_tries_orig[i])+str(noeuds_extr[i])+' [open traces/'+str(noeuds_tries_orig[i])+'-'+str(noeuds_extr[i])+'.tr w]\n')
 	script.write('$ns trace-queue $n('+str(noeuds_tries_orig[i])+') $n('+str(noeuds_extr[i])+') $file'+str(noeuds_tries_orig[i])+str(noeuds_extr[i])+'\n')
 	script.write('$ns queue-limit $n('+str(noeuds_tries_orig[i])+') $n('+str(noeuds_extr[i])+') 10\n\n')
 	i += 1
 
-# Ligne blanche
 script.write("\n")
-'''
-i = 0
-while i < nb_flux:
-	# script.write('set null('+str(noeuds_traf_src[i])+'_'+str(noeuds_traf_dst[i])+') [new Agent/Null]\n')
-	# script.write('$ns attach-agent $n('+str(noeuds_traf_src[i])+') $null('+str(noeuds_traf_src[i])+'_'+str(noeuds_traf_dst[i])+')\n')
-	# script.write('set tcp('+str(noeuds_traf_src[i])+'_'+str(noeuds_traf_dst[i])+') [new Agent/UDP]\n')
-	# script.write('$ns attach-agent $n('+str(noeuds_traf_dst[i])+') $tcp('+str(noeuds_traf_src[i])+'_'+str(noeuds_traf_dst[i])+')\n')
-	# script.write('$ns connect $tcp('+str(noeuds_traf_src[i])+'_'+str(noeuds_traf_dst[i])+') $null('+str(noeuds_traf_src[i])+'_'+str(noeuds_traf_dst[i])+')\n')
-	# script.write('$tcp('+str(noeuds_traf_src[i])+'_'+str(noeuds_traf_dst[i])+') set packetSize_ 1500\n\n')
-	# Envoi de données à un temps donné ns. En octets. at 0 "$tcp send 1000"
-	i += 1
-'''
+
 compteur_flux = 0
 somme_volume = 0
 while compteur_flux < nb_flux:
@@ -125,7 +96,7 @@ while compteur_flux < nb_flux:
 	for compteur_volume in liste_total_volume:
 		compteur_volume = compteur_volume.astype(int)
 		iterateur_sous_flux = 0
-		while somme_volume < compteur_volume*1024 :
+		while somme_volume < compteur_volume*1024*8 :
 			taille_segment_tcp = np.random.zipf(a, 1)
 
 			segmentSize = np.int(taille_segment_tcp[0])
@@ -140,9 +111,7 @@ while compteur_flux < nb_flux:
 			script.write('set ftp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') [new Application/FTP]\n')
 			script.write('$ftp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') set type_ FTP\n')
 			script.write('$ftp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') attach-agent $tcp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+')\n')
-			#script.write('$ns at '+str(rand_duree())+' "$ftp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') start"\n')
 			script.write('$ns at '+str(rand_duree())+' "$ftp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') send '+str(segmentSize)+'"\n\n')
-			#script.write('$ns at '+str(4.5)+' "$ftp('+str(noeuds_traf_src[compteur_flux])+'_'+str(noeuds_traf_dst[compteur_flux])+'_'+str(iterateur_sous_flux)+') stop"\n\n')
 
 			somme_volume = somme_volume + taille_segment_tcp[0]
 			somme_volume = np.uint64(somme_volume)
@@ -150,7 +119,7 @@ while compteur_flux < nb_flux:
 	somme_volume = 0
 	compteur_flux += 1
 
-script.write('$ns at 5.0 "finish"\n\n')
+script.write('$ns at 300.0 "finish"\n\n')
 script.write('# Run simulation\n')
 script.write('$ns run')
 
